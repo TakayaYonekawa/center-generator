4b52a89fe3887f048c591805d741eba74f5e2866
米川貴也 <zuomujianwu1@gmail.com>
2023-04-18 22:55:32 +0900

    404ページ追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import "./assets/scss/style.scss";
 import Header from "./component/templates/Header/Header";
 import Footer from "./component/templates/Footer/Footer";
 import Main from "./component/views/Main";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import MyPage from "./component/views/MyPage";
 import SaveCode from "./component/views/SaveCode";
+import NotFound from "./NotFound";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Main />}></Route>
         <Route path="/mypage" element={<MyPage />}></Route>
         <Route path={`/save-code/:savePageId`} element={<SaveCode />}></Route>
+        <Route element={<NotFound/>}/>
       </Routes>
       <Footer />
     </BrowserRouter>
